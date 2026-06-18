04f4ccec7a24d2790e82b77b2d7c82f2ba57c24b
ZerolesLuka <luka.lekarczyk28@gmail.com>
2026-06-18 17:56:58 -0400

    Add: guard sees the player
    Today I added a pretty core feature of my game, where the guard can see the player. Honestly I had some help with this but I can explain it perfectly. Basically I have a bool that takes the parameter of a player, it sets a bunch of fields for the guard and asks if the target is farther from range, return false, if the player is outside the guards angle return false, and if there's an obstacle inbetween them return false. That way, our performance for our game is much better because it isn't trying a million times a second because of the layer mask.

--- Assets/Scripts/GuardPatrol.cs
+++ Assets/Scripts/GuardPatrol.cs
@@ -6,7 +6,12 @@ public class GuardPatrol : NetworkBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Transform[] waypoints;
-    [SerializeField] private float reachDistance = 0.5f;
+    [SerializeField] private float reachDistance = 0.5f; //distance of which the guard consider it has reached waypoint
+
+    [SerializeField] private float sightRange = 10f;
+    [SerializeField] private float fovAngle = 120f; //field of view angle for the guard to see the player
+    [SerializeField] private float eyeHeight = 1.6f; //where the guard sees
+    [SerializeField] private LayerMask obstacleMask; //to check if there are obstacles between the guard and the player
 
 
     private int currentWaypoint = 0; //used in modulo
@@ -30,9 +35,28 @@ public class GuardPatrol : NetworkBehaviour
             currentWaypoint = (currentWaypoint + 1) % waypoints.Length; //add to the waypoint % means dont go over the lenght of waypoints, will reset to 0
             agent.SetDestination(waypoints[currentWaypoint].position); //agents destination is the current waypoints position
         }
+        foreach (Player p in FindObjectsByType<Player>(FindObjectsSortMode.None)) //for each player, if we can see them, log we see them
+            if (CanSeePlayer(p)) Debug.Log($"Guard sees {p.name}");
     }
     public void SetWaypoints(Transform[] points)
     {
-        waypoints = points;
+        waypoints = points; //set the waypoints from the spawner, since we cant set them in the inspector for the guard prefab
     }
+
+    private bool CanSeePlayer(Player target)
+    {
+        Vector3 eyePos = transform.position + Vector3.up * eyeHeight; //where the guard sees from
+        Vector3 targetPos = target.transform.position + Vector3.up * 1f; //where the player is, we can adjust this if we want the guard to see the player's head or body
+        Vector3 toTarget = targetPos - eyePos; //direction from guard to player
+        float distance = toTarget.magnitude; //length of the vector
+
+        if(distance > sightRange) return false; //In sight?
+        Vector3 dir = toTarget.normalized; //direction from guard to player normalized
+        if(Vector3.Angle(transform.forward, dir) > fovAngle * 0.5f) return false; //if player is outside of the guard's field of view, return false //in the cone?
+        if (Physics.Raycast(eyePos, dir, distance, obstacleMask)) return false; //if there's an obstacle between the guard and the player, return false
+
+        return true; //if we passed all the checks, we can see the player
+
+    }
+
 }
