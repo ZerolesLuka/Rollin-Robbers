65b4f4c284c83820d462413346e254207d494abf
ZerolesLuka <luka.lekarczyk28@gmail.com>
2026-06-20 10:59:53 -0400

    Add: guard state machine
    Added six states to the guard, for now when a player is within the guards noise radius, it adds a tick to a noiseCounter. A random int is chosen for the amount of noises the guard can "take" before he switches states, and that's all I got for now. Also the KeepCodeHere is like code for the guard to see the player through a cone vision, keeping it there for now bc I'll need it for later states.

--- Assets/Scripts/GuardPatrol.cs
+++ Assets/Scripts/GuardPatrol.cs
@@ -4,6 +4,15 @@ using UnityEngine.AI;
 
 public class GuardPatrol : NetworkBehaviour
 {
+    public enum GuardState { Asleep, Relaxed, Suspicious, Searching, Chasing, Caught }
+
+    [Networked] public GuardState State { get; private set; } //the guard's current state
+
+    [SerializeField] private float noiseRange = 4f; //how close a noise registers
+    private int noiseThreshold;
+    private int noiseCounter;
+    private float noiseTickTimer;
+
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Transform[] waypoints;
     [SerializeField] private float reachDistance = 0.5f; //distance of which the guard consider it has reached waypoint
@@ -12,7 +21,6 @@ public class GuardPatrol : NetworkBehaviour
     [SerializeField] private float fovAngle = 120f; //field of view angle for the guard to see the player
     [SerializeField] private float eyeHeight = 1.6f; //where the guard sees
     [SerializeField] private LayerMask obstacleMask; //to check if there are obstacles between the guard and the player
-
 
     private int currentWaypoint = 0; //used in modulo
 
@@ -23,20 +31,40 @@ public class GuardPatrol : NetworkBehaviour
             agent.enabled = false;
             return;
         }
+        State = GuardState.Asleep; //guard starts asleep
+        noiseThreshold = Random.Range(2, 5); //guard is triggered randomly
         agent.Warp(transform.position);
-        agent.SetDestination(waypoints[currentWaypoint].position); //set first destination
     }
 
     public override void FixedUpdateNetwork()
     {
         if(!HasStateAuthority) return; //only run for the state authority, which is the host in this case, so only the host will control the guard's movement
-        if (!agent.pathPending && agent.remainingDistance <= reachDistance) //if theres no path pending and we got to our destination
+
+        switch(State)
         {
-            currentWaypoint = (currentWaypoint + 1) % waypoints.Length; //add to the waypoint % means dont go over the lenght of waypoints, will reset to 0
-            agent.SetDestination(waypoints[currentWaypoint].position); //agents destination is the current waypoints position
+            case GuardState.Asleep:
+                if(HearsNoise())
+                {
+                    noiseTickTimer += Runner.DeltaTime; //if we heard noise, start the timer
+                    if (noiseTickTimer >= 1f) //if we heard noise for 1 second, increase the noise counter
+                    {
+                        noiseTickTimer = 0f; //reset timer
+                        noiseCounter++;
+                        if(noiseCounter >= noiseThreshold) //if we heard enough noise, wake up
+                        {
+                            Debug.Log("Guard woke up!");
+                            State = GuardState.Suspicious;
+                        }
+                    }
+
+                }
+            break;
+            case GuardState.Suspicious: 
+
+
         }
-        foreach (Player p in FindObjectsByType<Player>(FindObjectsSortMode.None)) //for each player, if we can see them, log we see them
-            if (CanSeePlayer(p)) Debug.Log($"Guard sees {p.name}");
+
+
     }
     public void SetWaypoints(Transform[] points)
     {
@@ -52,11 +80,30 @@ public class GuardPatrol : NetworkBehaviour
 
         if(distance > sightRange) return false; //In sight?
         Vector3 dir = toTarget.normalized; //direction from guard to player normalized
-        if(Vector3.Angle(transform.forward, dir) > fovAngle * 0.5f) return false; //if player is outside of the guard's field of view, return false //in the cone?
+        if(Vector3.Angle(transform.forward, dir) > fovAngle * 0.5f) return false; //in the cone?
         if (Physics.Raycast(eyePos, dir, distance, obstacleMask)) return false; //if there's an obstacle between the guard and the player, return false
 
         return true; //if we passed all the checks, we can see the player
+    }
+    private bool HearsNoise()
+    {
+        //STUB until hearing, any player within noiseRange will count as a noise, we can replace this with actual noise events later
+        foreach(Player p in FindObjectsByType<Player>(FindObjectsSortMode.None)) //for each player, if they're within noise range, count it as a noise
+          if(Vector3.Distance(transform.position, p.transform.position) <= noiseRange) 
+             return true;
+        return false;
+    }
 
+    private void KeepCodeHere()
+    {
+        if (!HasStateAuthority) return; //only run for the state authority, which is the host in this case, so only the host will control the guard's movement
+        if (!agent.pathPending && agent.remainingDistance <= reachDistance) //if theres no path pending and we got to our destination
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length; //add to the waypoint % means dont go over the lenght of waypoints, will reset to 0
+            agent.SetDestination(waypoints[currentWaypoint].position); //agents destination is the current waypoints position
+        }
+        foreach (Player p in FindObjectsByType<Player>(FindObjectsSortMode.None)) //for each player, if we can see them, log we see them
+            if (CanSeePlayer(p)) Debug.Log($"Guard sees {p.name}");
     }
 
 }
