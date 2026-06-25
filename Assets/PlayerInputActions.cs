b4164f1bd4a9ff9980787534b7375206801f70d2
ZerolesLuka <luka.lekarczyk28@gmail.com>
2026-06-25 11:51:04 -0400

    Add: player hand signals
    New Playerinputactions that read when the numbers 123 are pressed, these will eventually correlate to hand signals used when close to guard and don't wanna make noise, for now they log in the console.

--- Assets/PlayerInputActions.cs
+++ Assets/PlayerInputActions.cs
@@ -127,6 +127,33 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Signal1"",
+                    ""type"": ""Button"",
+                    ""id"": ""bd3ffed1-43a9-42d6-afc7-20003580dad8"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Signal2"",
+                    ""type"": ""Button"",
+                    ""id"": ""ea04ccf4-5ec8-4bbe-8109-7c0e341dd3fc"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Signal3"",
+                    ""type"": ""Button"",
+                    ""id"": ""50917cd2-05d5-4572-8bd9-390280a94550"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -215,6 +242,39 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Sprint"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a88965db-7dcc-4dce-ba7a-ffcf67374cee"",
+                    ""path"": ""<Keyboard>/1"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Signal1"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""14f872c2-ef0b-40e8-9072-67f2aba2762a"",
+                    ""path"": ""<Keyboard>/2"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Signal2"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""04da21ff-a6d1-47c0-996c-a47f78939cca"",
+                    ""path"": ""<Keyboard>/3"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Signal3"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -806,6 +866,9 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_Crouch = m_Player.FindAction("Crouch", throwIfNotFound: true);
         m_Player_Sprint = m_Player.FindAction("Sprint", throwIfNotFound: true);
+        m_Player_Signal1 = m_Player.FindAction("Signal1", throwIfNotFound: true);
+        m_Player_Signal2 = m_Player.FindAction("Signal2", throwIfNotFound: true);
+        m_Player_Signal3 = m_Player.FindAction("Signal3", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -903,6 +966,9 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_Crouch;
     private readonly InputAction m_Player_Sprint;
+    private readonly InputAction m_Player_Signal1;
+    private readonly InputAction m_Player_Signal2;
+    private readonly InputAction m_Player_Signal3;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -930,6 +996,18 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Sprint".
         /// </summary>
         public InputAction @Sprint => m_Wrapper.m_Player_Sprint;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Signal1".
+        /// </summary>
+        public InputAction @Signal1 => m_Wrapper.m_Player_Signal1;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Signal2".
+        /// </summary>
+        public InputAction @Signal2 => m_Wrapper.m_Player_Signal2;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Signal3".
+        /// </summary>
+        public InputAction @Signal3 => m_Wrapper.m_Player_Signal3;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -968,6 +1046,15 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
             @Sprint.started += instance.OnSprint;
             @Sprint.performed += instance.OnSprint;
             @Sprint.canceled += instance.OnSprint;
+            @Signal1.started += instance.OnSignal1;
+            @Signal1.performed += instance.OnSignal1;
+            @Signal1.canceled += instance.OnSignal1;
+            @Signal2.started += instance.OnSignal2;
+            @Signal2.performed += instance.OnSignal2;
+            @Signal2.canceled += instance.OnSignal2;
+            @Signal3.started += instance.OnSignal3;
+            @Signal3.performed += instance.OnSignal3;
+            @Signal3.canceled += instance.OnSignal3;
         }
 
         /// <summary>
@@ -991,6 +1078,15 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
             @Sprint.started -= instance.OnSprint;
             @Sprint.performed -= instance.OnSprint;
             @Sprint.canceled -= instance.OnSprint;
+            @Signal1.started -= instance.OnSignal1;
+            @Signal1.performed -= instance.OnSignal1;
+            @Signal1.canceled -= instance.OnSignal1;
+            @Signal2.started -= instance.OnSignal2;
+            @Signal2.performed -= instance.OnSignal2;
+            @Signal2.canceled -= instance.OnSignal2;
+            @Signal3.started -= instance.OnSignal3;
+            @Signal3.performed -= instance.OnSignal3;
+            @Signal3.canceled -= instance.OnSignal3;
         }
 
         /// <summary>
@@ -1319,6 +1415,27 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnSprint(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Signal1" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnSignal1(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Signal2" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnSignal2(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Signal3" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnSignal3(InputAction.CallbackContext context);
     }
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "UI" which allows adding and removing callbacks.
---
+++ Assets/Scripts/PlayerSignals.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using Fusion;
+
+public class PlayerSignals : NetworkBehaviour
+{
+    private Player player;
+
+    public override void Spawned()
+    {
+        player = GetComponent<Player>();
+    }
+    private void Update()
+    {
+        if(!HasInputAuthority || player.playerInputActions == null) //if not our player or has no control return
+        {
+            return;
+        }
+        if (player.playerInputActions.Player.Signal1.WasPressedThisFrame())
+        {
+            RPC_ShowSignal(1);
+        }
+        else if (player.playerInputActions.Player.Signal2.WasPressedThisFrame())
+        {
+            RPC_ShowSignal(2);
+        }
+        else if (player.playerInputActions.Player.Signal3.WasPressedThisFrame())
+        {
+            RPC_ShowSignal(3);
+        }
+    }
+    [Rpc(RpcSources.InputAuthority, RpcTargets.All)] //I press it, everyone runs this
+    private void RPC_ShowSignal(int id)
+    {
+        Debug.Log($"{name} signaled {id}"); //TEMP - later: trigger the matching finger gesture here
+    }
+
+}
